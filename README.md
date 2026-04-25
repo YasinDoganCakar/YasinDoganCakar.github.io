942bdafe13d1c22ba3c0d9a173a8d7d35a46a4b1
YasinDoganCakar <cakaryasindogan@gmail.com>
2026-04-25 09:54:36 +0300

    JsGeliştirme
    Click fonsktiyonları eklendi
    hedef metin ve rengi değiştiriliyor
    Oyun mekaniği hazır

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # YasinDoganCakar.github.io
+
+# Random Color Generator
+
+Bu projede ekranda dört farklı renkte kutu bulunmaktadır. Yukarıda   
+
+## 🚀 Özellikler
+- Rastgele renk seçimi
+- JavaScript ile dinamik değişim
+
+## 🛠️ Teknolojiler
+- HTML
+- CSS
+- JavaScript
---
+++ index.js
@@ -0,0 +1,38 @@
+// @ts-nocheck
+let score = 0;
+let colorRandom;
+
+function randomColorSelect() {
+    const colorArray = ["blue", "yellow", "red", "black"];
+    let colorRandomIndex = Math.floor(Math.random() * colorArray.length);
+    colorRandom = colorArray[colorRandomIndex];
+    return colorRandom;
+}
+
+function randomTextSelect() {
+    const textArray = ["Mavi", "Sarı", "Kırmızı", "Siyah"];
+    let textRandomIndex = Math.floor(Math.random() * textArray.length);
+    let textRandom = textArray[textRandomIndex];
+    return textRandom;
+}
+
+// Hedef yazının rengini ve textini değiştiriyor
+document.getElementById("key").style.color = randomColorSelect();
+document.getElementById("key").innerHTML = randomTextSelect();
+
+function clickBox(color) {
+    if (colorRandom === color) {
+        score++;
+    } else {
+        score--;
+    }
+
+    if(score === 0){
+        alert('Oyun Bitti');
+    }
+
+    document.getElementById("key").style.color = randomColorSelect();
+    document.getElementById("key").innerHTML = randomTextSelect();
+
+    document.getElementById("score").innerHTML = score;
+}
